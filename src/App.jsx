30bc653ad3bfc45474e07f05905fa3a9fa79b228
Deshan Charuka <d.c.0729439631@gmail.com>
2022-09-02 08:29:38 +0530

    Add combo box

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import ComboBox from "./ComboBox";
 
 const App = () => {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid xs={12}>
-            <div>1</div>
-          </Grid>
-          <Grid xs={12}>
-          <div>2</div>
-          </Grid>
-          <Grid xs={12}>
-          <div>3</div>
-          </Grid>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={12}>
+          <ComboBox />
         </Grid>
-      </Box>
-    
-    )
-}
+        <Grid xs={12}>
+          <div>2</div>
+        </Grid>
+        <Grid xs={12}>
+          <div>3</div>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
 export default App;
---
+++ src/ComboBox.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+
+const statusOptions = [
+  { label: "Completed", id: 1 },
+  { label: "Pending", id: 2 },
+  { label: "Not Started", id: 3 }
+];
+
+const ComboBox = () => {
+  return (
+    <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      options={statusOptions}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Status" />}
+    />
+  );
+};
+
+export default ComboBox;
