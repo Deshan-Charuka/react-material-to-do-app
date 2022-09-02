1a5f2ab738b7f097a9aff633359a70faf981961f
Deshan Charuka <d.c.0729439631@gmail.com>
2022-09-02 07:46:38 +0530

    Add material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+
+
 const App = () => {
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid xs={12}>
+            <div>1</div>
+          </Grid>
+          <Grid xs={12}>
+          <div>2</div>
+          </Grid>
+          <Grid xs={12}>
+          <div>3</div>
+          </Grid>
+        </Grid>
+      </Box>
+    
     )
 }
 
