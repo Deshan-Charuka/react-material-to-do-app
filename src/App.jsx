a164d219775e8b28c209af4a15c55edd73f24ec7
Deshan Charuka <d.c.0729439631@gmail.com>
2022-09-02 09:23:48 +0530

    Add task form

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
 import ComboBox from "./ComboBox";
+import InputForm from "./InputForm";
 
 const App = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} alignItems="center" justify="center">
         <Grid xs={12}>
           <ComboBox />
         </Grid>
@@ -15,7 +16,7 @@ const App = () => {
           <div>2</div>
         </Grid>
         <Grid xs={12}>
-          <div>3</div>
+          <InputForm />
         </Grid>
       </Grid>
     </Box>
---
+++ src/InputForm.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const defaultValues = {
+  taskDescription: "",
+};
+
+const InputForm = () => {
+  const [formValues, setFormValues] = useState(defaultValues);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        "& > :not(style)": { m: 1, width: "17ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="task-description-input"
+        label="Filled"
+        variant="filled"
+        name="taskDescription"
+        type="text"
+        value={formValues.taskDescription}
+        onChange={handleInputChange}
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Submit
+      </Button>
+    </Box>
+  );
+};
+
+export default InputForm;
